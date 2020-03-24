6a75a539d6a72506cb396a5700fae614fc345d4e
albertusbobby <50633957+albertusbobby@users.noreply.github.com>
2020-03-25 03:19:29 +0700

    add properties file

--- history/src/main/java/com/history/services/impl/HistoryServiceImpl.java
+++ history/src/main/java/com/history/services/impl/HistoryServiceImpl.java
@@ -2,6 +2,7 @@ package com.history.services.impl;
 
 import com.history.responses.GeneralResponse;
 import com.history.services.HistoryService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -9,11 +10,12 @@ import org.springframework.web.client.RestTemplate;
 @Service
 public class HistoryServiceImpl implements HistoryService {
 
+    @Value("${microservices.people.history.url}") private String peopleHistoryUrl;
+
     @Override
     public GeneralResponse getHistory(Long id) {
-        String url = "http://localhost:8081/people/history/"+id;
         RestTemplate restTemplate = new RestTemplate();
-        ResponseEntity<String> result = restTemplate.exchange(url, HttpMethod.POST, null, String.class);
+        ResponseEntity<String> result = restTemplate.exchange(peopleHistoryUrl+id, HttpMethod.POST, null, String.class);
         if(result!=null){
             return GeneralResponse.dialog(200, result.getBody());
         }else{
